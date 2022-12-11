7c5a3adc43010e66dfb9585555e797af298ace31
RustyGithubb <98985426+RustyGithubb@users.noreply.github.com>
2022-12-11 23:57:48 +0100

    [Aled]Add menu pause + lvl test build + plein de choses

---
+++ Source/TheDigII/InputControllerUI.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class THEDIGII_API InputControllerUI
+{
+public:
+	InputControllerUI();
+	~InputControllerUI();
+};
---
+++ Source/TheDigII/TheDigII.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TheDigII.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TheDigII, "TheDigII" );
